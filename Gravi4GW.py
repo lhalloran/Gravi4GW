3611db3f8eddcc9cc4b1c7f28aa26146be95861a
lhalloran <landon.halloran@gmail.com>
2020-11-23 09:25:49 +0100

    now single file with all functions (Gravi4GW)

--- Gravi4GW.py
+++ Gravi4GW.py
@@ -18,18 +18,128 @@ import matplotlib.pyplot as plt
 import scipy.interpolate as interp
 from osgeo import gdal
 #from matplotlib.colors import LightSource
-import G4GW_f # could turn this into a class-style python package...
+#import G4GW_f
+
+# define constants (all in SI units)
+G=6.67408E-11 # gravitational constant
+rho_H2O = 1000 # density of water
+
+def quickplotterxyz(x,y,z):
+    # quickplotterxyz(x,y,z)
+    # Simple plotting of x,y,z arrays for debugging.
+    # x,y,z : 2-D arrays of x, y, and elevation
+    fig, axs = plt.subplots(nrows=1,ncols=3,sharex=True,sharey=True,figsize=(12,6)) 
+    axs[0].imshow(x); axs[0].set_title('x, min='+str(int(min(x.flatten())))+', max='+str(int(max(x.flatten()))))
+    axs[1].imshow(y); axs[1].set_title('y, min='+str(int(min(y.flatten())))+', max='+str(int(max(y.flatten()))))
+    axs[2].imshow(z); axs[2].set_title('z, min='+str(int(min(z.flatten())))+', max='+str(int(max(z.flatten()))))
+
+def point_maker(stn_x,stn_y,max_r,nr,dens_rad=8):
+    # point_maker(stn_x,stn_y,max_r,nr,dens_rad=8)
+    #
+    # Returns x,y coordinates the element area represented by each point. Point distribution is 
+    # radial with increasing distance from centre for each subsequent radius. Even radial spacing 
+    # for each r.
+    # 
+    # Parameters:
+    # stn_x, stn_y : coordinates of centre point
+    # max_r : distance of furthest points from centre point
+    # nr : number of radii (including r=0) for point creation
+    # dens_rad : (optional) point density for first non-zero radius, must be multiple of 4
+    #
+    if dens_rad%4 != 0:
+        print('#Gravi4GW: point_maker requires a radial density parameter dens_rad that is a multiple of 4')
+        return 0
+    rs=np.append(np.array(0.0),np.logspace(np.log10(0.1), np.log10(max_r), num=nr-1))
+    log_fac=np.log10(rs[-1]/rs[-2])
+    rsextra=rs[-1]*10**log_fac # for calculation of elemental areas of farthest points
+    n=rs-rs
+    n=dens_rad + 4*np.floor_divide(rs,10) #number of points for given r, increasing with radius
+    n[0]=1 # one point at centre
+    A = rs-rs # area represent by each point at given r
+    for i in np.arange(1,nr-1):
+        r_out = (rs[i+1]+rs[i])/2 # outer radius of element
+        r_in = (rs[i]+rs[i-1])/2 # inter radius of element
+        A[i] = np.pi*(r_out**2-r_in**2)/n[i]
+    A[0] = np.pi*((rs[0]+rs[1])/2)**2
+    A[-1]=np.pi*((rsextra+rs[-1])**2-(rs[-1]+rs[-2])**2)/(4*n[-1])
+    # make r,phi coordinates, referenced to centre point
+    pt_r=np.array([])
+    pt_phi=np.array([])
+    pt_A=np.array([])
+    for i in np.arange(nr):
+        for j in np.arange(n[i]):
+            pt_r = np.append(pt_r,rs[i])
+            #print(i)
+            #print(pt_r)
+            pt_phi = np.append(pt_phi,2*np.pi*j/n[i])
+            pt_A=np.append(pt_A,A[i])
+    # convert to x,y cords:
+    x=stn_x+np.multiply(pt_r,np.cos(pt_phi))
+    y=stn_y+np.multiply(pt_r,np.sin(pt_phi))
+    return x,y,pt_A
+
+def dg(xyz_stn,xyz_pt,dm,debug=False):
+    # dg(xyz_stn,xyz_pt,dm,debug=False) is a function that returns the delta gravity vector for a station point and 
+    # a grid point, given a change in mass of dm at the point
+    #
+    # xyz_stn is local coordiates (in m) of gravity station. numpy array of size 3.
+    # xyz_pt is local coordiates (in m) of point on GW table grid. numpy array of size 3.
+    # dm is (near infinitessimal) change in mass of water at the point on the grid
+    # (optional) debug=True will print internal values for each call of the function
+    dxyz = xyz_pt-xyz_stn;
+    dx=dxyz[0]
+    dy=dxyz[1]
+    dz=dxyz[2]
+    d = np.sqrt(dx**2+dy**2+dz**2) #distance between station and point
+    dg1 = G*dm/d**2
+    theta = np.arctan2(dz,np.sqrt(dx**2+dy**2)) # angle below xy surface
+    phi = np.arctan2(dy,dx)    
+    dgx=dg1*np.cos(theta)*np.sin(phi)
+    dgy=dg1*np.cos(theta)*np.cos(phi)
+    dgz=dg1*np.sin(theta)
+    dgxyz=np.array([dgx,dgy,dgz])
+    if(debug):
+        print(dxyz)
+        print([d,dg1,theta,phi])
+        print(dgxyz)
+    return dgxyz
+
+def hillshade(array,azimuth,angle_altitude):
+    # Adapted from:
+    # github.com/rveciana/introduccion-python-geoespacial/blob/master/hillshade.py
+    azimuth = 360.0 - azimuth 
+
+    x, y = np.gradient(array)
+    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
+    aspect = np.arctan2(-x, y)
+    azimuthrad = azimuth*np.pi/180.
+    altituderad = angle_altitude*np.pi/180.
+
+    shaded = np.sin(altituderad)*np.sin(slope) + np.cos(altituderad)*np.cos(slope)*np.cos((azimuthrad - np.pi/2.) - aspect)
+
+    return 255*(shaded + 1)/2
 
 
-def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=30, single_or_array='single'):
-    # Gravi4GW()
-    # 
-    # Arguments:
-    # 
-    # Optional arguments:
-    #   n_r: number of radial distances for point definition in numerical integral (default = 30)
-    #   accept_resid: approximate acceptable residual based on Bouger plate approximation (see paper)
-    #   
+def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=30, do_figs=True):
+    """ 
+    Gravi4GW.Gravi4GW()
+    
+    Arguments:
+        tif_path: string 
+            Path to the geotiff file (e.g. DEM or water table elevation model).
+            Coordinate-system must be meter-based (i.e., not degrees).
+        gravstn_x, gravstn_y: array-like or scalar 
+            The x and y coordinate(s) of the stations in the same coordinate systems as the 
+            geotiff at tif_path. If one or both of these arrays has length > 1, a grid is formed
+            at all x,y pairs.
+        GW_depth: scalar
+            The estimated vertical distance between the gravity sensor location and the water 
+            table.
+     Optional arguments:
+         accept_resid: approximate acceptable residual based on Bouger plate approximation (see paper)
+         n_r: number of radial distances for point definition in numerical integral (default = 30)
+         do_figs: boolean to enable creation of figures.
+    """
     DEM_in = gdal.Open(tif_path, gdal.GA_ReadOnly) 
     print('#Gravi4GW: DEM file '+str(tif_path)+' imported. Size = '+str(DEM_in.RasterXSize)+' x '+str(DEM_in.RasterYSize))
     DEM_z = np.array(np.float64(DEM_in.ReadAsArray()))
@@ -42,11 +152,11 @@ def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=3
     DEM_y = DEM_geotransform[3] + DEM_geotransform[4]*xy_inds[0] + DEM_geotransform[5]*xy_inds[1]
     DEM_x=DEM_x.transpose()
     DEM_y=DEM_y.transpose()
-    G4GW_f.quickplotterxyz(DEM_x,DEM_y,DEM_z)
+    if do_figs:
+        quickplotterxyz(DEM_x,DEM_y,DEM_z)
 
     # create interpolation function
     print('#Gravi4GW: Creating DEM interpolation function...')
-    # ...TO DO HERE???: cut out portion of input data that is relevant to points (decrease size of input to RectBivariateSpline)
     interp_spline = interp.RectBivariateSpline(DEM_x[0,:],-DEM_y[:,0],DEM_z.transpose()) # x,y must be strictly increasing, hence - sign on y
 
     stn_array_size = [np.size(gravstn_x),np.size(gravstn_y)]
@@ -56,9 +166,8 @@ def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=3
     stn_y = stn_y.flatten()
     stn_z = stn_x-stn_x
 
-
     for i in np.arange(nstns):
-        stn_z[i] = interp_spline(stn_x[i],-stn_y[i]) + 0.5
+        stn_z[i] = interp_spline(stn_x[i],-stn_y[i]) #+ 0.5
     
     max_r = GW_depth/accept_resid # max radius from stn for calculation (see notes)
     dataproc = []
@@ -81,14 +190,14 @@ def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=3
         # create sampling points and calulate DEM at these points
         print('#Gravi4GW: Defining sampling points and calculating interpolated DEM at these points...')
     
-        xx,yy,AA= G4GW_f.point_maker(stn_xyz[0],stn_xyz[1],max_r,n_r,dens_rad=8)
+        xx,yy,AA = point_maker(stn_xyz[0],stn_xyz[1],max_r,n_r,dens_rad=8)
         npts=np.size(xx)
         zz=xx-xx
         for i in np.arange(npts):
             zz[i]=interp_spline(xx[i],-yy[i])
-        if n==0: # plot of points for integration (only do this once)
+        if n==0 and do_figs: # plot of points for integration (only do this once)
             plt.figure(figsize=(10,8)); plt.scatter(xx,yy,c=zz,s=AA,alpha=0.5); plt.axis('equal'); plt.title(str(npts)+' Integration Points'); plt.colorbar()# plot points
-        
+            
         # Calculate dg/dH by numerical integration:
         dH=0.01 # drop in water table in equivalent H2O (equivalent to delta hydraulic head / porsity). Should be small so as to estimate dG/dH.
         dgxyz_sum = np.array([0,0,0])
@@ -98,11 +207,11 @@ def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=3
             if int(100*i/npts)-progresspct >=10: # print progress
                 progresspct=int(100*i/npts)
                 print('#Gravi4GW: Integration progress = '+str(progresspct)+'%')
-            dm = dH*G4GW_f.rho_H2O*AA[i]
+            dm = dH*rho_H2O*AA[i]
             pt_xyz = np.array([xx[i],yy[i],zz[i]-GW_d])
-            dgxyz_sum = dgxyz_sum + G4GW_f.dg(stn_xyz,pt_xyz,dm)
-        dg = np.append(dgxyz_sum,np.sqrt(dgxyz_sum[0]**2+dgxyz_sum[1]**2+dgxyz_sum[2]**2))
-        dgdH = dg/dH
+            dgxyz_sum = dgxyz_sum + dg(stn_xyz,pt_xyz,dm)
+        dg1 = np.append(dgxyz_sum,np.sqrt(dgxyz_sum[0]**2+dgxyz_sum[1]**2+dgxyz_sum[2]**2))
+        dgdH = dg1/dH
         dgdH_uGal = dgdH*1E8 #in microGal/mH2O
         dataproc.append([stn_xyz[0],stn_xyz[1],stn_xyz[2],GW_d,dgdH_uGal[0],dgdH_uGal[1],dgdH_uGal[2],dgdH_uGal[3]])
         print('#Gravi4GW: dg_x = ' + str(dgdH_uGal[0]) + ' uGal/mH2O')
@@ -112,39 +221,40 @@ def Gravi4GW(tif_path, gravstn_x, gravstn_y, GW_depth, accept_resid=0.025, n_r=3
     dataproc=np.array(dataproc) #convert data to numpy array
 
     #%% plot the results
-    # maybe improve this using https://matplotlib.org/3.1.1/gallery/specialty_plots/topographic_hillshading.html
-    fig, axs = plt.subplots(nrows=1,ncols=2,sharex=True,sharey=True,figsize=(15,8))
-    DEM_hs = G4GW_f.hillshade(DEM_zC,45,20)
-    #ls = LightSource(azdeg=315, altdeg=45)
-    cbobj1 = axs[0].imshow(np.flipud(DEM_hs),cmap='Greys',alpha=1, interpolation='bilinear',extent=[DEM_xC[0,0],DEM_xC[0,-1],DEM_yC[0,0],DEM_yC[-1,0]])
-    demobj = axs[0].contourf(DEM_xC,DEM_yC,DEM_zC,cmap='gist_earth',alpha=0.5, levels=80)
-    plt.gca().invert_yaxis()
-    #cbobj1 = axs[0].contourf(DEM_xC,DEM_yC,DEM_hs,cmap='Greys',alpha=0.5,levels=40)
-    #axs[0].pcolor(DEM_xC,DEM_yC,DEM_hs,cmap='Greys',alpha=0.5,linewidth=0,rasterized=True)
-    #for c in cbobj1.collections:
-    #    c.set_edgecolor("face")
-    #    c.set_alpha(0.5)
-    
-    axs[0].set_title('Input DEM (m)')
-    axs[0].set_aspect('equal')
-    axs[0].grid(c='k')
-    plt.setp(axs[0].get_xticklabels(), rotation=45)
-    plt.setp(axs[0].get_yticklabels(), rotation=45)
-    
-    cbarax1 = fig.add_axes([0.48, 0.2, 0.01, 0.6])
-    fig.colorbar(demobj, cax=cbarax1, orientation='vertical')
-    
-    levels=np.linspace(np.min(dataproc[:,-1]),41.93*2-np.min(dataproc[:,-1]),101)
-    #cbobj2 = axs[1].contourf(stn_x_array, stn_y_array, dataproc[:,-1].reshape(stn_array_size),levels=levels,cmap='bwr')
-    cbobj2 = axs[1].contourf(gravstn_x, gravstn_y, dataproc[:,-1].reshape(np.flip(stn_array_size)),levels=levels,cmap='bwr')
-    for c in cbobj2.collections:
-        c.set_edgecolor("face")
-    axs[1].set_title('dg/dH ($\mu$Gal/mH$_2$O)')
-    axs[1].set_aspect('equal')
-    axs[1].grid(c='k')
-    plt.gca().invert_yaxis()
-    plt.setp(axs[1].get_xticklabels(), rotation=45)
-    cbarax2 = fig.add_axes([0.91, 0.2, 0.01, 0.6])
-    fig.colorbar(cbobj2, cax=cbarax2, orientation='vertical')
+    if do_figs:
+        # maybe improve this using https://matplotlib.org/3.1.1/gallery/specialty_plots/topographic_hillshading.html
+        fig, axs = plt.subplots(nrows=1,ncols=2,sharex=True,sharey=True,figsize=(15,8))
+        DEM_hs = hillshade(DEM_zC,45,20)
+        #ls = LightSource(azdeg=315, altdeg=45)
+        cbobj1 = axs[0].imshow(np.flipud(DEM_hs),cmap='Greys',alpha=1, interpolation='bilinear',extent=[DEM_xC[0,0],DEM_xC[0,-1],DEM_yC[0,0],DEM_yC[-1,0]])
+        demobj = axs[0].contourf(DEM_xC,DEM_yC,DEM_zC,cmap='gist_earth',alpha=0.5, levels=80)
+        plt.gca().invert_yaxis()
+        #cbobj1 = axs[0].contourf(DEM_xC,DEM_yC,DEM_hs,cmap='Greys',alpha=0.5,levels=40)
+        #axs[0].pcolor(DEM_xC,DEM_yC,DEM_hs,cmap='Greys',alpha=0.5,linewidth=0,rasterized=True)
+        #for c in cbobj1.collections:
+        #    c.set_edgecolor("face")
+        #    c.set_alpha(0.5)
+        
+        axs[0].set_title('Input DEM (m)')
+        axs[0].set_aspect('equal')
+        axs[0].grid(c='k')
+        plt.setp(axs[0].get_xticklabels(), rotation=45)
+        plt.setp(axs[0].get_yticklabels(), rotation=45)
+        
+        cbarax1 = fig.add_axes([0.48, 0.2, 0.01, 0.6])
+        fig.colorbar(demobj, cax=cbarax1, orientation='vertical')
+        
+        levels=np.linspace(np.min(dataproc[:,-1]),41.93*2-np.min(dataproc[:,-1]),101)
+        #cbobj2 = axs[1].contourf(stn_x_array, stn_y_array, dataproc[:,-1].reshape(stn_array_size),levels=levels,cmap='bwr')
+        cbobj2 = axs[1].contourf(gravstn_x, gravstn_y, dataproc[:,-1].reshape(np.flip(stn_array_size)),levels=levels,cmap='bwr')
+        for c in cbobj2.collections:
+            c.set_edgecolor("face")
+        axs[1].set_title('dg/dH ($\mu$Gal/mH$_2$O)')
+        axs[1].set_aspect('equal')
+        axs[1].grid(c='k')
+        plt.gca().invert_yaxis()
+        plt.setp(axs[1].get_xticklabels(), rotation=45)
+        cbarax2 = fig.add_axes([0.91, 0.2, 0.01, 0.6])
+        fig.colorbar(cbobj2, cax=cbarax2, orientation='vertical')
 
     return dataproc
--- demo.py
+++ demo.py
@@ -1,8 +1,7 @@
+import Gravi4GW
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.interpolate as interp
-from osgeo import gdal
-import Gravi4GW, G4GW_f
+import pandas as pd
+
 
 # import DEM (must be in a meter-based projection)
 #DEM_path = 'DEMs/Tsalet_30m_DEM_EPSG2056.tif'
@@ -10,12 +9,18 @@ DEM_path = 'DEMs/RechyDEM_swisstopo_2m.tif'
 #DEM_path = 'DEMs/Salar_de_Uyuni_DEM.tif'
 #DEM_path = 'DEMs/Maules_Creek_20m_DEM-H.tif'
 # define x,y of gravity station
-GW_d = 15 # assumed depth to water table from ground surface
-stn_x_array = 2606292 + 50*np.arange(-5,5)
+GW_d = 4 # assumed depth to water table from ground surface
+stn_x_array = 2606292 + 50*np.arange(-8,8)
 stn_y_array = 1116278 + 50*np.arange(-5,5)
 #stn_x_array = 1327850 + 30*np.arange(-20,20)
 #stn_y_array = -2236035.0 + 30*np.arange(-20,20)
 #stn_x_array = 1508000+ 20*np.arange(-20,20)
 #stn_y_array = -3523500.0+ 20*np.arange(-20,20)
 
-output = Gravi4GW.Gravi4GW(DEM_path, stn_x_array, stn_y_array, GW_d, accept_resid=0.025, n_r=40, single_or_array='array')
+file_out = 'Output/data_out.csv'
+
+output = Gravi4GW.Gravi4GW(DEM_path, stn_x_array, stn_y_array, GW_d, accept_resid=0.02, n_r=40, do_figs=True)
+
+dfcols = ['x [m]','y [m]','z [m]','stn. height above GW table [m]','dg/dH (x-component) [uGal/m]','dg/dH (y-component) [uGal/m]','dg/dH (z-component) [uGal/m]','dg/dH [uGal/m]']
+outputdf = pd.DataFrame(data=output,columns=dfcols)
+outputdf.to_csv(file_out,index=False)
