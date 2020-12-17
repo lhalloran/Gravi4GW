5ed26f7129e2f1245e4baada957c5040a1133e5b
lhalloran <landon.halloran@gmail.com>
2020-12-17 20:38:27 +0100

    removed unnecessary files

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # Gravi4GW
-Landon Halloran (www.ljsh.ca) 2021
+by Landon Halloran (www.ljsh.ca) 2021
 
 ## Overview
+Gravi4GW (pronounced Gra-vee-for-ground-water) is a python tool that enables the calculation of the conversion factor between changes in gravity (&Delta;g) and changes in groundwater storage (GWSC). The tool accepts topographic or groundwater table data in the geotiff format. 
 
 ## How to cite.
+The paper is:
+Halloran, L.J.S. (under review) "Improving groundwater storage change estimates using time-lapse gravimetry with Gravi4GW".
 
 ## How to use. 
+For a demonstration of usage, see `demo.py`.
+
+Basic usage:
+```
+import Gravi4GW
+# ...
+# define DEM_path, stn_x_array, stn_y_array, h_eff
+# ...
+output = Gravi4GW.Gravi4GW(DEM_path, stn_x_array, stn_y_array, h_eff, accept_resid=0.02, n_r=40, do_figs=True)
+```
+
--- demo.py
+++ demo.py
@@ -2,20 +2,12 @@ import Gravi4GW
 import numpy as np
 import pandas as pd
 
-
-# import DEM (must be in a meter-based projection)
-#DEM_path = 'DEMs/Tsalet_30m_DEM_EPSG2056.tif'
+# define path to DEM or GW elevation model (must be in a meter-based projection)
 DEM_path = 'DEMs/RechyDEM_swisstopo_2m.tif'
-#DEM_path = 'DEMs/Salar_de_Uyuni_DEM.tif'
-#DEM_path = 'DEMs/Maules_Creek_20m_DEM-H.tif'
-# define x,y of gravity station
+
 GW_d = 4 # assumed depth to water table from ground surface
-stn_x_array = 2606457 + 20*np.arange(-20,20)
-stn_y_array = 1116119 + 20*np.arange(-20,20)
-#stn_x_array = 1327850 + 30*np.arange(-20,20)
-#stn_y_array = -2236035.0 + 30*np.arange(-20,20)
-#stn_x_array = 1508000+ 20*np.arange(-20,20)
-#stn_y_array = -3523500.0+ 20*np.arange(-20,20)
+stn_x_array = 2606457 + 20*np.arange(-10,10)
+stn_y_array = 1116119 + 20*np.arange(-10,10)
 
 file_out = 'Output/data_out.csv'
 
